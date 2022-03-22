5af26d22f70496d33dbb947912fdfa8370e5bcd7
Space <94127295+SpaceYes@users.noreply.github.com>
2022-03-22 22:16:10 +0800

    Create Emoji.lua

---
+++ Emoji.lua
@@ -0,0 +1,44 @@
+-- [[ Coded By Space#3245 You Cunt! ]] --
+
+local StarId = {
+	[60935213] = true,
+}
+
+local CrownId = {}
+
+local DiamondId = {
+	[2819726490] = true,
+	[953043143] = true,
+	[99087422] = true,
+}
+
+function premium()
+	for _,v in pairs(game:GetService('Players'):GetChildren()) do
+		if StarId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
+				end
+			end
+		elseif CrownId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]'..v.DisplayName)
+				end
+			end
+		elseif DiamondId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]'..v.DisplayName)
+				end
+			end
+		elseif v.Character then
+			if v.Character.Parent.Name == 'Players' then
+				if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🙂]'..v.DisplayName)
+				end
+			end
+		end
+	end
+end
+local success,err = pcall(premium)
