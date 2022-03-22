f84f50923b887f0291d318f1965862d3fb9b65ec
Space <94127295+SpaceYes@users.noreply.github.com>
2022-03-22 22:15:09 +0800

    Create Banned.lua

---
+++ Banned.lua
@@ -0,0 +1,9 @@
+-- [[ Coded By Space#3245 You Cunt! ]] --
+
+local function Banned(UserID, Reason)
+	if game:GetService('Players').LocalPlayer.UserId == UserID then
+		game:GetService('Players').LocalPlayer:Kick('RAYX BANNED : '..Reason)
+	end
+end
+
+Banned(2985941001, 'Banned ALT')
